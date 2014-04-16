f9a4b496fe60143e76ac9bd71a28207c3191bdf5
ALS <sinyawskiy@gmail.com>
2014-04-16 22:10:33 +0400

    Update jquery.slideControl.js
    update some orphography, remove ie fix

--- jquery.slideControl.js
+++ jquery.slideControl.js
@@ -37,23 +37,13 @@
 			var handle = fill.find('.slideControlHandle');
 			var input = parent.find('input');
 			var containerWidth = container.outerWidth() + 1;
-			var handleWidth = $(handle).outerWidth();
-			var offset = $(container).offset();
+			var handleWidth = handle.outerWidth();
 			var animate = function(value){$(fill).animate({ width: value + "%"}, o.speed);}
-			
-			$(window).resize(function() {
-				offset = $(container).offset();
-			});
-			
-			//adds shadow class to handle for IE <9
-			if (getInternetExplorerVersion() < 9 && getInternetExplorerVersion() > -1) {
-				handle.addClass('ieShadow');
-			}
 			
 			// when user clicks anywhere on the slider
 			$(container).click(function(e) {		
 				e.preventDefault();
-				position = checkBoundaries(Math.round(((e.pageX - offset.left + handleWidth/2)/containerWidth)*100));
+				position = checkBoundaries(Math.round(((e.pageX - $(this).offset().left + handleWidth/2)/containerWidth)*100));
 				
 				animate(position);
 				$(input).val(position/10);
@@ -65,7 +55,7 @@
 				controller = true;
 				$(document).mousemove(function(e) {
 					e.preventDefault();
-					position = checkBoundaries(Math.round(((e.pageX - offset.left + handleWidth/2)/containerWidth)*100));
+					position = checkBoundaries(Math.round(((e.pageX - container.offset().left + handleWidth/2)/containerWidth)*100));
 					if (controller) {	
 						$(fill).width(position + "%");
 						$(input).val(position/10);
@@ -80,10 +70,11 @@
 			// when user changes value in input
 			$(input).change(function() {
 				var value = checkBoundaries($(this).val()*10);
-				if ($(this).val() > o.upperBound)
+				if ($(this).val() > o.upperBound){
 					$(input).val(o.upperBound);
-				else if ($(this).val() < o.lowerBound)
+				}else if ($(this).val() < o.lowerBound){
 					$(input).val(o.lowerBound);
+				}
 				animate(value);
 			});
 			
@@ -91,25 +82,15 @@
 		
 		// checks if value is within boundaries
 		function checkBoundaries(value) {
-			if (value < options.lowerBound*10)
+			if (value < options.lowerBound*10){
 				return options.lowerBound*10;
-			else if (value > options.upperBound*10)
+			}else if (value > options.upperBound*10){
 				return options.upperBound*10;
-			else
+			}else{
 				return value;
+			}
 		}
 		
-		// checks ie version
-		function getInternetExplorerVersion(){
-		   var rv = -1;
-		   if (navigator.appName == 'Microsoft Internet Explorer') {
-			  var ua = navigator.userAgent;
-			  var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-			  if (re.exec(ua) != null)
-				 rv = parseFloat( RegExp.$1 );
-		   }
-		   return rv;
-		}
 		return this;
 	 }
 })(jQuery);
